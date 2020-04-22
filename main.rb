a72f3299c94bd0e825f18f9f42c85eb3fe59b52e
Stephen Allard <stephencallard@gmail.com>
2020-04-22 04:26:08 -0500

    Added main.rb

--- main.rb
+++ main.rb
@@ -1,4 +1,18 @@
 require "./lib/node.rb"
 require "./lib/linked_list.rb"
 
-list1 = Linkedlist.new
+list1 = LinkedList.new
+list1.append("stephen")
+list1.append("regan")
+list1.append("alex")
+list1.append("mike")
+
+puts list1.to_s
+
+
+
+
+
+
+
+
